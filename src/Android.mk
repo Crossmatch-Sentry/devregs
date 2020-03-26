fe58056af4074d9d726e33ff7b890d3473083f49
David Kilp <David.Kilp@crossmatch.com>
2020-03-26 16:37:05 -0400

    When building on Android disable compiler warning -Wno-unused-variable

--- src/Android.mk
+++ src/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := eng
 LOCAL_SRC_FILES:=devregs.cpp
 LOCAL_MODULE:=devregs
-LOCAL_CPPFLAGS += -DANDROID
+LOCAL_CPPFLAGS += -DANDROID -Wno-unused-variable
 LOCAL_SHARED_LIBRARIES:=libutils libc libstdc++
 LOCAL_C_INCLUDES += $(LOCAL_PATH)
 include $(BUILD_EXECUTABLE)
